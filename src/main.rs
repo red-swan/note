be8dfff754bd8b3a3146c7d1ca59a88546462847
red-swan <redswan@outlook.com>
2025-11-06 09:20:46 -0700

    Add open command

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
 use std::fs::File;
 use std::io::Write;
 use std::env;
+use opener::open;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-      panic!("Did not specify a note!")
+      return Err("Did not specify a note!".into());
     }
 
     let output_path = env::home_dir().unwrap().join("notes.temp");
-    println!("{:?}", output_path); 
 
-    // if args.len() == 2 && args[1] == "open" {
-    //   std::fs::File::open(output_path)
-    // }
+    if args.len() == 2 && args[1] == "open" {
+      open(&output_path)?;
+      return Ok(());
+    }
 
     let arg_line = args[1..].join(" ");
     let mut f = File::options().append(true).create(true).open(output_path)?;
-    writeln!(&mut f, "{:?}", arg_line)?;
+    writeln!(&mut f, "{}", arg_line)?;
     Ok(())
 }
